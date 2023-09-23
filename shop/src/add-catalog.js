c7f55338f126a8d507e49abad95c5662b9f92cb8
Albert Labarento <albertlabarento@gmail.com>
2023-09-23 12:37:23 +0800

    master: Cleanup

---
+++ shop/src/add-catalog.js
@@ -0,0 +1,6 @@
+import { addCatalog } from "catalogs/store";
+
+const randomId = () => (Math.random() + 1).toString(36).substring(7);
+document.getElementById("addCatalog").addEventListener("click", () => {
+    addCatalog(`New Catalog: ${randomId()}`);
+});
--- shop/src/bootstrap.js
+++ shop/src/bootstrap.js
@@ -1,12 +1,8 @@
 import mountCatalog from "catalogs/mount";
-import { subscribe, addCatalog } from "catalogs/store";
+import { subscribe } from "catalogs/store";
+import './add-catalog';
 
 mountCatalog("#shop-catalogs");
 subscribe((payload) => {
     console.log("Catalogs has been updated!", payload?.catalogs);
-});
-
-const randomId = () => (Math.random() + 1).toString(36).substring(7);
-document.getElementById("addCatalog").addEventListener("click", () => {
-    addCatalog(`New Catalog: ${randomId()}`);
 });
